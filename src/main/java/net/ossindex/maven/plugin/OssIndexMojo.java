1862d1c858cffcfee56188d12740daa05cbabf90
Ken Duck <ken@qracorp.com>
2015-09-27 21:12:30 -0400

    Initial commit

---
+++ src/main/java/net/ossindex/maven/plugin/OssIndexMojo.java
@@ -0,0 +1,54 @@
+/**
+ *	Copyright (c) 2015 Vör Security Inc.
+ *	All rights reserved.
+ *	
+ *	Redistribution and use in source and binary forms, with or without
+ *	modification, are permitted provided that the following conditions are met:
+ *	    * Redistributions of source code must retain the above copyright
+ *	      notice, this list of conditions and the following disclaimer.
+ *	    * Redistributions in binary form must reproduce the above copyright
+ *	      notice, this list of conditions and the following disclaimer in the
+ *	      documentation and/or other materials provided with the distribution.
+ *	    * Neither the name of the <organization> nor the
+ *	      names of its contributors may be used to endorse or promote products
+ *	      derived from this software without specific prior written permission.
+ *	
+ *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *	ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *	DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.ossindex.maven.plugin;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/** Cross reference the project against information in OSS Index to identify
+ * security and maintenance problems.
+ * 
+ * @author Ken Duck
+ *
+ */
+@Mojo( name = "ossindex")
+public class OssIndexMojo extends AbstractMojo
+{
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.apache.maven.plugin.Mojo#execute()
+	 */
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException
+	{
+		getLog().info( "Hello, world." );
+	}
+
+}
